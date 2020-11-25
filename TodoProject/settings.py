8bd82dd278016d66358964e59c174dc9a955c9ff
yuzua <k.saito70329@gmail.com>
2020-11-25 14:23:24 +0900

    2020/11/25

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -127,9 +127,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Static files (CSS, JavaScript, Images)
