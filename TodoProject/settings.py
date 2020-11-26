7132549b181517dedbc3e6dad14420bf32d93693
yuzua <63239669+yuzua@users.noreply.github.com>
2020-11-26 11:57:51 +0900

    Update settings.py

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgrespass',
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['POSTGRES_HOST'],
         'PORT': 5432,
     }
